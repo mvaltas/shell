4df2e3799ae56c5987e4468e6c74c1231e3a9972
Marco Valtas <mavcunha@gmail.com>
2025-11-21 21:56:45 -0800

    Adds godot LSP

--- dotfiles/nvim/lua/extra/godot.lua
+++ dotfiles/nvim/lua/extra/godot.lua
@@ -1,3 +1,12 @@
+
+
+vim.lsp.config('gdscript', {
+  filetypes = { 'gdscript' },
+  root_markers = { '.godot' },
+})
+vim.lsp.enable('gdscript')
+
+
 local godot_group = vim.api.nvim_create_augroup('GodotGroup', { clear = true })
 
 vim.api.nvim_create_autocmd('FileType', {
